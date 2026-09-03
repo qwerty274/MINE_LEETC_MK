8e09bb2b224242e4c6203a1c031400f3885be760
i_MK <harhome2121@gmail.com>
2026-09-04 02:43:35 +0530

    Time: 6 ms (98.67%), Space: 70.9 MB (74.29%) - LeetHub

---
+++ 0221-maximal-square/0221-maximal-square.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int maximalSquare(char[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int dp[][] = new int[m][n];
+        for(int i =0;i<m;i++){
+            if(matrix[i][0] == '1'){
+                dp[i][0] =1;
+            }
+            else{
+                dp[i][0] = 0;
+            }
+        }
+        for(int j = 0;j<n;j++){
+             if(matrix[0][j] == '1'){
+                dp[0][j] =1;
+            }
+            else{
+                dp[0][j] = 0;
+            }
+        }
+        for(int i =1;i<m;i++){
+            for(int j = 1;j<n;j++){
+                 if(matrix[i][j] == '1'){
+                    dp[i][j] = Math.min(dp[i][j-1],Math.min(dp[i-1][j],dp[i-1][j-1]))+1;
+                 }
+                 else{
+                    dp[i][j] = 0;
+                 }
+            }
+        }
+        int max = 0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(dp[i][j]>max){
+                    max=dp[i][j];
+                }
+            }
+        }
+        return max *max;
+    }
+}
+//1 0 1 0 0
+//1 0 1 1 1 
+//1 1 1 2 2 
+//1 1 1 2 2
+//1 0 0 1 0
